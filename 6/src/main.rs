b47fad492c116e0810305e4beb845f9fb96f38f6
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-12-06 09:08:57 -0500

    trying reverse order for d6

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -11,14 +11,14 @@ fn main() {
     for line in text.lines() {
         let stars: Vec<&str> = line.split(")").collect();
 
-        orbit_hierarchy.insert(stars[1].to_string(), stars[0].to_string());
-        has_parent.insert(stars[0].to_string());
+        orbit_hierarchy.insert(stars[0].to_string(), stars[1].to_string());
+        has_parent.insert(stars[1].to_string());
     }
 
     // TODO: rethink direction, might need to go other way to do count properly
 
     let mut total = 0;
-    let mut counted = HashMap::new();
+    let mut counted: HashMap<String, usize> = HashMap::new();
     for (star, child_star) in &orbit_hierarchy {
         if !has_parent.contains(star) {
             println!("star {}", star);
@@ -27,13 +27,13 @@ fn main() {
             let mut child_star = child_star;
             loop {
                 if let Some(child) = orbit_hierarchy.get(child_star) {
-                    if counted.contains_key(child) {
-                        break;
-                    }
+                    // if counted.contains_key(child) {
+                    //     break;
+                    // }
                     count += 1;
                     total += count;
                     child_star = child;
-                    counted.insert(child);
+                // counted.insert(child);
                 } else {
                     break;
                 }
