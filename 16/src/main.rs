2cb4dc5a4e0682920c000c96db3a9800629a828f
Aaron McLeod <aaron.g.mcleod@gmail.com>
2020-10-21 18:59:09 -0400

    part 2 setup

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -1,12 +1,12 @@
 use read_input::read_text;
 
-fn main() {
-    let text = read_text("16/input.txt").unwrap();
-    let mut input_list: Vec<i32> = text
-        .chars()
+fn get_initial(text: &String) -> Vec<i32> {
+    text.chars()
         .map(|c| c.to_digit(10).unwrap() as i32)
-        .collect();
+        .collect()
+}
 
+fn fft(mut input_list: Vec<i32>) -> Vec<i32> {
     let pattern = vec![0, 1, 0, -1];
 
     for _ in 0..100 {
@@ -31,6 +31,31 @@ fn main() {
 
         input_list = output;
     }
+
+    input_list
+}
+
+fn main() {
+    let text = read_text("16/input.txt").unwrap();
+    let input_list = fft(get_initial(&text));
+
+    println!(
+        "{:?}",
+        input_list[0..8]
+            .iter()
+            .map(|n| n.to_string())
+            .collect::<String>()
+    );
+
+    let initial: Vec<i32> = get_initial(&text);
+
+    let mut input_list: Vec<i32> = Vec::with_capacity(input_list.len() * 10000);
+
+    for _ in 0..10000 {
+        input_list.append(&mut initial.clone());
+    }
+
+    let input_list = fft(input_list);
 
     println!(
         "{:?}",
